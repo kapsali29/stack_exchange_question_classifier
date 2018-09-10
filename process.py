1b7bf0bae14ca216db9190ba14dcd9fbb06109bd
Panagiotis Kapsalis <kapsalis@synelixis.com>
2018-09-10 17:34:28 +0300

    some changes

--- process.py
+++ process.py
@@ -1,4 +1,12 @@
 import json
+import nltk
+
+from nltk import RegexpTokenizer
+
+try:
+    from nltk.corpus import stopwords
+except:
+    nltk.download('stopwords')
 
 
 def preprocess_data(filepath):
@@ -16,3 +24,24 @@ def preprocess_data(filepath):
             train_labels.append(json_formatted["topic"])
             train_data.append(json_formatted["question"] + " " + json_formatted["excerpt"])
         return train_labels, train_data
+
+
+def split_to_unigrams(train_data):
+    """
+    Using that function we can split the text (questions and excerpts) to unigrams
+    :param train_data: train dataset
+    :return: splitted texts
+    """
+    tokenizer = RegexpTokenizer(r'\w+')
+    stop_words = set(stopwords.words('english'))
+    processed_data = []
+    for text in train_data:
+        words = tokenizer.tokenize(text)
+        processed_data.append(
+            [word.lower() for word in words if
+             word not in stop_words and word != "" and not word.isdigit()])
+    return processed_data
+
+
+labels, train_data = preprocess_data("training.json")
+print(split_to_unigrams(train_data)[0])
